45fd79ad06f64ad3a60d26a4c1edda0bc374cfb4
jtrobinson1993@gmail.com <jtrobinson1993@gmail.com>
2017-09-01 20:25:44 -0500

    Header, menu

--- web/src/js/app.js
+++ web/src/js/app.js
@@ -1,10 +1,54 @@
-window.onload = function() {
-  //querySelectorAll doesn't really return an array, it returns something 'array-like', this uses some fancy ES6 syntax [... ] to turn it into a real array
-  const $ = elem => [...document.querySelectorAll(elem)];
 
-  const $mainMenuIcon = $('#main-header .menu-icon');
+const OPEN_CLASS = 'open';
 
-  console.log('hello world');
+/* Create a jQuery-like selector
+querySelectorAll doesn't really return an array, it returns something
+'array-like', this uses some fancy ES6 syntax [... ] to turn it into
+a real array
+*/
+const $ = sel => document.querySelector(sel);
+const $$ = sel => [...document.querySelectorAll(sel)];
 
-  console.log($mainMenuIcon);
+function toggleClass(elem, c) {
+    const classes = elem.className;
+    /* Capturing Groups are enclosed in parentheses().
+    ?: Prevents the capturing group from being referenced later on, which
+    stops the .replace from getting rid of spaces before the class name, 'c'.
+    ^ matches the start of a string
+    | means "or"
+    \\s means "whitespace"
+    ?:^|\\s means "Match on whitespace or start of string, but don't
+    save the capturing group in memory".
+    The '+' after the ')' means "one or many characters", meaning that
+    our class name, 'c', will contain one or many characters before the
+    next regex.
+    ?! essentially means "Is not followed by:"
+    and \S (escaped with an extra \) means non-white space characters
+    (negated version of \s).
+    so ?!\\S means "Not followed by non-whitespace characters".
+    */
+    const openRegex = new RegExp('(?:^|\\s)+'+ c +'(?!\\S)');
+
+    if (classes.indexOf(c) === -1) {
+      elem.className += ' ' + c;
+    } else {
+      elem.className = classes.replace(openRegex, '');
+    }
 }
+
+function setupMainMenu() {
+  const $mainMenuIcon = $('#main-header .menu-icon');
+  const $mainMenu = $('#main-menu');
+
+  $mainMenuIcon.addEventListener('click', () => {
+    toggleClass($mainMenu, OPEN_CLASS);
+    toggleClass($mainMenuIcon, OPEN_CLASS);
+    $mainMenuIcon.blur();
+  });
+}
+
+function init() {
+  setupMainMenu();
+}
+
+init();
